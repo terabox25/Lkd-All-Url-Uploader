6e7202df2a9f13d8f1bd8f6c0c5372327c4616a9
terabox25 <164458249+terabox25@users.noreply.github.com>
2024-08-25 22:51:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import logging
-from pyrogram.raw.all import layer
-from pyrogram import Client, yt-dlp, idle, __version__
+from pyrogram.raw.all import layer, yt-dlp
+from pyrogram import Client, idle, __version__
 from config import Config
 
 logging.basicConfig(
